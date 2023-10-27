ae8fe166a2bb911df27406739c37013c494b3dfe
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-10-28 00:11:23 +0300

    Добавил решение к заданию 1 в главе 4

--- chapter_04/task_01/main.go
+++ chapter_04/task_01/main.go
@@ -1,7 +1,17 @@
 package main
 
+import "fmt"
+
 // Упражнение по программированию 4.1
 //
 // Напишите программу, которая запрашивает имя и фамилию, а затем выводит их в формате <i>имя, фамилия</i>
 func main() {
+	var first_name string
+	var second_name string
+
+	fmt.Print("Введите имя и фамилию: ")
+
+	fmt.Scan(&first_name, &second_name)
+
+	fmt.Printf("%s %s", first_name, second_name)
 }
