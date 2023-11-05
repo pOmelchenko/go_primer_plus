098aa893c67260f00702654a6f428975c8dabd96
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-11-05 18:54:01 +0300

    Добавил решение к заданию 7 в главе 4

--- chapter_04/task_07/main.go
+++ chapter_04/task_07/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Упражнение по программированию 4.7
 //
 // Напишите программу, которая присваивает переменной типа <code>double</code> значение 1.0/3.0 и переменной
@@ -8,5 +10,9 @@ package main
 // Включите также в программу заголовочный файл <code>float.h</code> и выведите значение <code>FLT_DIG</code> и
 // <code>DBL_DIG</code>. Согласуются ли выведенные значения со значением 1.0/3.0?
 func main() {
+	var f_res float32 = 1.0 / 3.0
+	var d_res float64 = 1.0 / 3.0
 
+	fmt.Printf("float32:  %.4f %.12f %.16f\n", f_res, f_res, f_res)
+	fmt.Printf("float64: %.4f %.12f %.16f\n", d_res, d_res, d_res)
 }
