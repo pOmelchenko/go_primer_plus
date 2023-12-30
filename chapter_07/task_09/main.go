d35f3c3d15b7257a2bbf5533c073a9199fad9464
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-12-30 23:48:10 +0300

    Добавил решению к заданию 9 в главе 7

---
+++ chapter_07/task_09/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+// Упражнение по программированию 7.9
+//
+// Напишите программу, которая принимает в качестве ввода положительное целое число и отображает все простые
+// числа, которые меньше или равны введенному числу.
+func main() {
+	var (
+		num   uint32
+		div   uint32
+		prime uint32
+	)
+
+	fmt.Print("Введите число: ")
+
+	for _, err := fmt.Scan(&num); err == nil; _, err = fmt.Scan(&num) {
+		count := 0
+		for prime = 2; prime <= num; prime++ {
+			isPrime := true
+
+			for div = 2; div*div <= prime; div++ {
+				if prime%div == 0 {
+					isPrime = false
+				}
+			}
+
+			if isPrime {
+				count++
+				fmt.Printf("%5d", prime)
+
+				if count%10 == 0 {
+					fmt.Println()
+				}
+			}
+		}
+
+		fmt.Println()
+		fmt.Println("Введите следующее число для анализа, или введите q для завершения.")
+	}
+
+	fmt.Println("До свидания")
+}
